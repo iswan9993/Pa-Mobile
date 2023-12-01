8ab9b007cfeb13a633fa04c180b29ce434af4e96
Fela afria ningsih <113352248+Felaafrianingsih1@users.noreply.github.com>
2023-12-01 09:15:39 +0800

    Update bottom_navbar.dart

--- flutter_application_1/lib/navbar/bottom_navbar.dart
+++ flutter_application_1/lib/navbar/bottom_navbar.dart
@@ -1,5 +1,9 @@
+
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter_application_1/cart/cart.dart';
+import 'package:flutter_application_1/cart/cartscreen.dart';
+import 'package:flutter_application_1/tools/tools.dart';
 
 class Navbar extends StatelessWidget {
   @override
@@ -10,20 +14,29 @@ class Navbar extends StatelessWidget {
       animationDuration: Duration(milliseconds: 300),
       items: [
         Icon(
-          Icons.home, 
-          color: Colors.white
+          Icons.home,
+          color: Colors.white,
+        ),
+        GestureDetector(
+          onTap: () {
+            // Navigate to the CartScreen (cartApp)
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CartScreen(),
+              ),
+            );
+          },
+          child: Icon(
+            Icons.restaurant,
+            color: Colors.white,
           ),
+        ),
         Icon(
-          Icons.restaurant, 
-          color: Colors.white
-          ),
-        Icon(
-          Icons.settings, 
-          color: Colors.white
-          )
+          Icons.settings,
+          color: Colors.white,
+        )
       ],
     );
   }
 }
-
-
