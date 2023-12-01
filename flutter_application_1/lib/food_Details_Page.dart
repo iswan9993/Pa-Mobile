df6a8cbf1f2cb3b906889931c60d5d7171f69588
Fela afria ningsih <113352248+Felaafrianingsih1@users.noreply.github.com>
2023-12-01 09:14:40 +0800

    Update food_Details_Page.dart

--- flutter_application_1/lib/food_Details_Page.dart
+++ flutter_application_1/lib/food_Details_Page.dart
@@ -1,44 +1,53 @@
 import 'package:flutter/material.dart';
-import "package:flutter_application_1/tools/tools.dart";
+import 'package:flutter_application_1/cart/cartscreen.dart';
+import 'package:flutter_application_1/tools/tools.dart';
+
 
 class FoodDetailsPage extends StatelessWidget {
   final Food food;
 
- const FoodDetailsPage({Key? key, required this.food}) : super(key : key);
+  const FoodDetailsPage({Key? key, required this.food}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(food.name),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Tambahkan makanan ke keranjang saat tombol "Add to Cart" diklik
+              addToCart(context, food);
+            },
+          ),
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-         Padding(
-          padding:const EdgeInsets.all(16.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: Image.asset(
-              food.imagepath,
-              width: 400,
-              height: 250,
-              fit: BoxFit.cover,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.asset(
+                food.imagepath,
+                width: 400,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          ),
-
           SizedBox(
             height: 16,
           ),
           Text(
             food.name,
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 35,
-              color: Colors.lightBlueAccent
-            ),
+                fontWeight: FontWeight.bold,
+                fontSize: 35,
+                color: Colors.lightBlueAccent),
           ),
           Text('PRICE: ${food.price}'),
           Padding(
@@ -50,11 +59,34 @@ class FoodDetailsPage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20,
                 color: Colors.brown,
-              ),),
-            )
-
-          // Tambahkan detail lainnya sesuai kebutuhan
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Tambahkan makanan ke keranjang saat tombol "Add to Cart" diklik
+              addToCart(context, food);
+            },
+            child: Text('Add to Cart'),
+          ),
         ],
+      ),
+    );
+  }
+
+  void addToCart(BuildContext context, Food food) {
+    // Implementasi logika penambahan makanan ke keranjang
+    // Misalnya, Anda dapat menggunakan provider atau cara lainnya untuk mengelola keranjang
+    // Di sini, kita hanya menambahkan item ke CartScreen
+    CartScreen.cartItems.add(food);
+
+    // Tampilkan snackbar atau informasi lainnya jika diperlukan
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${food.name} ditambahkan ke keranjang'),
       ),
     );
   }
