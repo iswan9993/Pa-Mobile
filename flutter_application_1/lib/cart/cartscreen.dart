34bc3bd21dc34561c4ec88cc8aa3dad01356ae58
Fela afria ningsih <113352248+Felaafrianingsih1@users.noreply.github.com>
2023-12-01 09:13:15 +0800

    Update cartscreen.dart

--- flutter_application_1/lib/cart/cartscreen.dart
+++ flutter_application_1/lib/cart/cartscreen.dart
@@ -1,34 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/food_Details_Page.dart';
 import 'package:flutter_application_1/tools/tools.dart';
-import 'package:flutter/material.dart';
+
+class CartItem {
+  final String name;
+  final String price;
+  final String imagepath;
+  final String ket;
+
+  CartItem({
+    required this.name,
+    required this.price,
+    required this.imagepath,
+    required this.ket,
+  });
+}
 
 class CartScreen extends StatelessWidget {
-  final List<Food> cartItems;
-
-  CartScreen({required this.cartItems});
+  static List<Food> cartItems = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple.shade200,
       appBar: AppBar(
-        title: Text(''),
+        title: Text('Keranjang Sushi'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Kembali ke halaman sebelumnya
+          },
+        ),
       ),
-      body: ListView.builder(
-        itemCount: cartItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(cartItems[index].name),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('\$${cartItems[index].price.toString()}'),
-                Text(cartItems[index].ket),
-              ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          // Tambahkan widget atau desain lainnya di sini sesuai kebutuhan
+
+          // Gunakan ListView.builder untuk menampilkan gambar dari setiap item di keranjang
+         
+
+          // Daftar item di keranjang
+          Expanded(
+            child: ListView.builder(
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(cartItems[index].name),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('\$${cartItems[index].price}'),
+                      Text(cartItems[index].ket),
+                    ],
+                  ),
+                  leading: Image.asset(
+                    cartItems[index].imagepath,
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
