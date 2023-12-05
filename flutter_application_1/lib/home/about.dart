55ee2db9bf62d9bb6401b475da4ecbae23e5364c
Fela afria ningsih <113352248+Felaafrianingsih1@users.noreply.github.com>
2023-12-05 20:29:46 +0800

    Add files via upload

--- flutter_application_1/lib/home/about.dart
+++ flutter_application_1/lib/home/about.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 147, 125, 187),
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 147, 125, 187),
         title: Text('About Us'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Restoran Sushi',
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Deskripsi tentang restoran sushi dan informasi lainnya.',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Kelas B1 Kelompok 6',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            ),
-            Text('Masha Jacinda Putri '),
-            Text('Fela Afria Ningsih'),
-            Text('Iswan'),
-            SizedBox(height: 16.0),
-          ],
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage(
+                  'lib/images/street-market.png',
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Restoran Sushi',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Aplikasi Pemesanan Sushi.',
+                style: TextStyle(fontSize: 16.0),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Kelas B1 Kelompok 6',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Text('Masha Jacinda Putri '),
+              Text('Fela Afria Ningsih'),
+              Text('Iswan'),
+              SizedBox(height: 16.0),
+            ],
+          ),
         ),
       ),
     );
