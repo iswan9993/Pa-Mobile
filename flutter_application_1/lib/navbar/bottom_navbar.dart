1fb4b3d3b0a4fe7010c004705bf95279a2fb1aec
Fela afria ningsih <113352248+Felaafrianingsih1@users.noreply.github.com>
2023-12-07 20:19:26 +0800

    Update bottom_navbar.dart

--- flutter_application_1/lib/navbar/bottom_navbar.dart
+++ flutter_application_1/lib/navbar/bottom_navbar.dart
@@ -1,16 +1,17 @@
-
-import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:flutter_application_1/cart/cart.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_application_1/cart/cartscreen.dart';
-import 'package:flutter_application_1/tools/tools.dart';
+import 'package:flutter_application_1/home/favorite.dart';
+import 'package:flutter_application_1/home/history_page.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/favoriteprovider.dart';
 
 class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CurvedNavigationBar(
-      backgroundColor: Colors.deepPurple.shade200,
-      color: Colors.deepPurple,
+      backgroundColor: Color.fromARGB(255, 147, 125, 187),
+      color: Color.fromARGB(255, 78, 48, 138),
       animationDuration: Duration(milliseconds: 300),
       items: [
         Icon(
@@ -19,7 +20,7 @@ class Navbar extends StatelessWidget {
         ),
         GestureDetector(
           onTap: () {
-            // Navigate to the CartScreen (cartApp)
+            // Navigate to the CartScreen
             Navigator.push(
               context,
               MaterialPageRoute(
@@ -28,14 +29,43 @@ class Navbar extends StatelessWidget {
             );
           },
           child: Icon(
-            Icons.restaurant,
+            Icons.shopping_cart,
             color: Colors.white,
           ),
         ),
-        Icon(
-          Icons.settings,
-          color: Colors.white,
-        )
+        GestureDetector(
+          onTap: () {
+            // Navigate to the HistoryPage
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HistoryPage(
+                  historyItems: CartScreen.historyItems,
+                ),
+              ),
+            );
+          },
+          child: Icon(
+            Icons.history,
+            color: Colors.white,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            // Navigate to the FavoritePage
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    FavoritePage(), // Ganti FavoritePage dengan nama halaman favorit Anda
+              ),
+            );
+          },
+          child: Icon(
+            Icons.favorite,
+            color: Colors.white,
+          ),
+        ),
       ],
     );
   }
